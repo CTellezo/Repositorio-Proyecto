224ab42fc550ef5c1a5b1bed8afe4616de223a47
Sierra Prometida <rt5960369@gmail.com>
2023-10-24 20:07:12 -0600

    Estoy cansaadohermano

--- Quiz/js1.js
+++ Quiz/js1.js
@@ -1,15 +1,21 @@
 //Validar formulario
 let btnEvaluar = document.querySelector("#evaluar");
 btnEvaluar.onclick=()=>{
-    var correctas=[1 ,2 ,3 ,4,1 ,2 ,3 ,4,1 ,2 ,3 ,4,1 ,2 ,3 ,4,1 ,2 ,3 ,4,1 ,2 ,3 ,4,1 ,2 ,3 ,4,1 ,2 ,3 ,4,1 ,2 ,3 ,4,1 ,2 ,3 ,4];
+    var correctas=[1 ,4 ,3 ,2,4 ,1 ,4 ,4,1 ,1];
     var aciertos=0;
-for(i=1; i<=10; i++){
+for(i=1; i<=7; i++){
     let r1=document.querySelector("#p" +i+ "r1");
     let r2=document.querySelector("#p" +i+ "r2");
     let r3=document.querySelector("#p" +i+ "r3");
     let r4=document.querySelector("#p" +i+ "r4");
+    let r5=document.querySelector("#p" +i+ "r5");
+    let r6=document.querySelector("#p" +i+ "r6");
+    let r7=document.querySelector("#p" +i+ "r7");
+    let r8=document.querySelector("#p" +i+ "r8");
+    let r9=document.querySelector("#p" +i+ "r9");
+    let r10=document.querySelector("#p" +i+ "r10");
 
-if(!r1.checked && !r2.checked && !r3.checked && !r4.checked){
+if(!r1.checked && !r2.checked && !r3.checked && !r4.checked ){
     Swal.fire({
         icon:'error',
         title:'ERROR',
@@ -24,10 +30,10 @@ if(!r1.checked && !r2.checked && !r3.checked && !r4.checked){
     }}
 
 
-    if(aciertos>=3){
-        Swal.fire({icon:'success',title:'Gracias por contestar',});
+    if(aciertos>=7){
+        Swal.fire({icon:'success',title:'Exelente, Tienes buenas bases en la musica',});
     }else{
-        Swal.fire({icon:'success',title:'Gracias por contestar',text:'  ',footer:'Quiz'});
+        Swal.fire({icon:'success',title:'Estudia mas',text:'  ',footer:'Quiz'});
     }
 }
 
@@ -38,7 +44,8 @@ btnBorrar.onclick=()=>{
         document.querySelector("#p" +i+ "r1").checked=false;
         document.querySelector("#p" +i+ "r2").checked=false;
         document.querySelector("#p" +i+ "r3").checked=false;
-        document.querySelector("#p" +i+ "r4").checked=false;                        
+        document.querySelector("#p" +i+ "r4").checked=false;  
+                            
     }
 }
 
---
+++ js/Registro.js
@@ -0,0 +1,7 @@
+function Mostrar(params) {
+    var nombre= document.querySelector("#nombre").value;
+    var apellido= document.querySelector("#apellido").value;
+    var correo= document.querySelector("#correo").value;
+    var contraseña= document.querySelector("#contraseña").value;
+    
+}
