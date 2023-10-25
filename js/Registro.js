d1287d9c9699c68212ad0d838ffa7d898bf1b1eb
Sierra Prometida <rt5960369@gmail.com>
2023-10-25 03:21:31 -0600

    MI PRIMERA CHAMBAAAAAAA

--- js/Registro.js
+++ js/Registro.js
@@ -1,7 +1,24 @@
-function Mostrar(params) {
-    var nombre= document.querySelector("#nombre").value;
-    var apellido= document.querySelector("#apellido").value;
-    var correo= document.querySelector("#correo").value;
-    var contraseña= document.querySelector("#contraseña").value;
-    
+function obtenerDatosUsuario() {
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var sexo = document.getElementById("sexo").value;
+    var instrumento = document.getElementById("Instrumento").value;
+    var  correo= document.getElementById("correo").value;
+    var contraseña = document.getElementById("contraseña").value;
+  
+    return [nombre, apellido, sexo, instrumento, correo, contraseña];
 }
+function mostrarDatosEnConsola(datos) {
+    console.log("Nombre: " + datos[0]);
+    console.log("Apellido: " + datos[1]);
+    console.log("Sexo: " + datos[2]);
+    console.log("Instrumento: " + datos[3]);
+    console.log("Correo: " + datos[4]);
+    console.log("Contraseña: " + datos[5]);
+}
+
+function obtenerYMostrarDatos() {
+    var datos = obtenerDatosUsuario();
+    mostrarDatosEnConsola(datos);
+}
+
